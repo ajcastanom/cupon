6d8d205ed156e3f820a4cc7d1265cab74518a2f2
anderson <anderson@Debian>
2014-02-07 20:43:04 -0500

    Avance hasta entorno routing

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,21 +3,28 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function portadaAction()
+    public function portadaAction($ciudad = null)
     {
+        if($ciudad == null){
+            $ciudad = $this->container->getParameter('cupon.ciudad.por_defecto');
+            return new RedirectResponse(
+                    $this->generateUrl('portada', array('ciudad' => $ciudad))
+                    );
+        }
         $em = $this->getDoctrine()->getManager();
         
-        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-            'ciudad'    =>  1,
-            'fechaPublicacion'  =>  new \DateTime('today')
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findBy(array(
+            'id' => 1,
+            'ciudad' => $ciudad
         ));
         
         return $this->render(
                 'OfertaBundle:Default:portada.html.twig',
-                array('oferta' =>   $oferta)
+                array('oferta' =>   $oferta[0])
         );
     }
 }
